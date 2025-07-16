02872a5e10bbe73d0b0f38ba0ffb5a93b4eb70a1
Zengard <maskanda54@yandex.ru>
2025-07-16 23:39:02 +0300

    Movement animation
    Add movement animation to player and also set up player script a little bit

---
+++ Assets/_Scripts/Player/AnimatorHashes.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class AnimatorHashes
+{
+    public static int MovingStateHash = Animator.StringToHash("IsMoving");
+    public static int AtackStateHash = Animator.StringToHash("");//TODO: Add string for attack
+}
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -6,16 +6,22 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput _playerInput;
     private InputMaster _inputMaster;
 
-   [SerializeField] private float _speed;
+    [SerializeField] private float _speed;
     Vector2 _inputVector;
+
+    private Animator _animator;
+    private bool _isMoving;
+   
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
 
-        _inputMaster =new InputMaster();
+        _inputMaster = new InputMaster();
         _inputMaster.Player.Enable();
         //_inputMaster.Player.Movement.performed += Movement_performed;
+
     }
 
     private void Update()
@@ -26,6 +32,8 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         transform.position += new Vector3(_inputVector.x, 0, _inputVector.y).normalized * _speed * Time.fixedDeltaTime;
+
+        AnimationMovement();
     }
 
     private void Movement_performed(InputAction.CallbackContext context)
@@ -33,5 +41,21 @@ public class PlayerMovement : MonoBehaviour
         Vector2 inputVector = context.ReadValue<Vector2>();
 
         transform.position += new Vector3(inputVector.x, 0, inputVector.y) * _speed * Time.deltaTime;
+    }
+
+    private void AnimationMovement()
+    {
+        if (_inputVector.magnitude > 0.1f || _inputVector.magnitude < -0.1f) 
+            _isMoving = true;
+        else
+            _isMoving = false;
+
+        if(_isMoving) 
+        {
+            _animator.SetFloat("X", _inputVector.x);
+            _animator.SetFloat("Y", _inputVector.y);
+        }
+
+        _animator.SetBool(AnimatorHashes.MovingStateHash, _isMoving);
     }
 }
